8aaaafc7e2960b717ff178fd253db763a2f5c7a8
karitotp <karito@developmentseed.org>
2019-01-21 13:41:05 -0500

    comparing labels

--- index.js
+++ index.js
@@ -8,61 +8,60 @@ var imagesAttr = {};
 
 
 
-fs.readFile(__dirname + '/right.xml', function (err, data) {
+fs.readFile(__dirname + '/right_buildings.xml', function (err, data) {
 	parser.parseString(data, function (err, result) {
 
 		for (var i = 0; i < result.annotations.image.length; i++) {
 			images[result.annotations.image[i].$.id] = result.annotations.image[i];
 		}
 
-
 		for (var indexstr in images) {
 			var currentImagen = images[indexstr];
 			var currentImagenBoxes = currentImagen.box;
 			var index = parseInt(indexstr);
-			imagesAttr[indexstr]=[];
+			imagesAttr[indexstr] = [];
 			for (let d = 0; d < currentImagenBoxes.length; d++) {
 				const box = currentImagenBoxes[d];
 				var attr = fix_attributes(box.attribute);
 				imagesAttr[indexstr].push(attr);
 			}
-			// for (let i = 1; i < 6; i++) {
-
-			// 	if (images[index + i]) {
-			// 		console.log('======= next imagens ' + (index + i))
-			// 		console.log(images[index + i])
-			// 	}
-			// }
 		}
 
+		var weird = 0
+		for (i = 1; i < Object.keys(imagesAttr).length; i++) {
+			var previewElement = Object.values(imagesAttr)[i - 1];
+			var element = Object.values(imagesAttr)[i];
+			var nextElement = Object.values(imagesAttr)[i + 1];
+			// console.log(previewElement)
+			// console.log(element)
+			// console.log(nextElement)
+			if ((JSON.stringify(previewElement.sort()) !== JSON.stringify(element.sort())) && (JSON.stringify(element.sort()) !== JSON.stringify(nextElement.sort()))) {
+				weird++;
+				console.log('They are no equal!');
+				var a = Object.entries(imagesAttr)[i - 1];
+				var b = Object.entries(imagesAttr)[i];
+				var c = Object.entries(imagesAttr)[i + 1];
+				console.log(a)
+				console.log(b)
+				console.log(c)
 
-		//============================================0000
-		// for (var index in imagesAttr) {
-
-		// 	console.log(imagesAttr[index]);
-		
-		// }
-
-		console.log(imagesAttr)
-
+			}
+		}
+		console.log("Diferencias: "+weird)
 	});
 });
 
-
-function fix_attributes(attributes){
- var fix_attr ={}
+function fix_attributes(attributes) {
+	var fix_attr = {}
 
 	for (let i = 0; i < attributes.length; i++) {
 		const attribute = attributes[i];
 		// console.log('=======')
-	var key = attribute.$.name;
-	var value = attribute._;
+		var key = attribute.$.name;
+		var value = attribute._;
 
-	// console.log(key,value)
-	fix_attr[key]=value;
-
-		
+		// console.log(key,value)
+		fix_attr[key] = value;
 	}
-return `${fix_attr.material}-${fix_attr.design}-${fix_attr.construction}` 
-
+	return `${fix_attr.material}-${fix_attr.design}-${fix_attr.construction}`
 }
